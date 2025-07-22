b8bd6ef645a9d2740f0bf9ab28347a7f3d783100
Janckus <hussein_barakat2020@outlook.com>
2025-07-22 10:54:50 +0100

    First major push
    Balance + Bug fix + Added heavy arty

--- common/defines/ww1_defines.lua
+++ common/defines/ww1_defines.lua
@@ -1,9 +1,8 @@
-
 NDefines.NGame.START_DATE = "1911.1.1.12"
 NDefines.NGame.END_DATE = "1950.1.1.1"
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 60				-- Days of client lag for decrease of gamespeed
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 60				-- Days of client lag for pause of gamespeed.
-NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.15, 0.05, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
+NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.15, 0.04, 0.0 } -- game speeds for each level. Must be 5 entries with last one 0 for unbound
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 999			--Max army experience a country can store
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 999			--Max navy experience a country can store
 NDefines.NMilitary.MAX_AIR_EXPERIENCE = 999				--Max air experience a country can store
@@ -11,3 +10,5 @@ NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 40  		-- how many divisions a
 NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 40			-- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5				-- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
 NDefines.NMilitary.BASE_NIGHT_ATTACK_PENALTY = -0.5
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 3.5		-- Speed at which militaryfactories produce equipment
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0		-- Cost of changing a brigade in a division
