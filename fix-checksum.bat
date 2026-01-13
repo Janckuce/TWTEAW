0e92cc8a080af2a91348f8c5e972e69f318d1a07
Janckus <hussein_barakat2020@outlook.com>
2026-01-13 22:43:08 +0000

    Revert "Test 2"
    This reverts commit 9504b0591fbd86ce4ae069ab91ae5faa8f1501f9.

--- fix-checksum.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Fixing line endings to match workshop version...
-git config core.autocrlf false
-git config core.safecrlf true
-git rm -rf --cached .
-git reset --hard HEAD
-echo.
-echo Done! Restart HOI4 and your checksum should match now.
-pause
